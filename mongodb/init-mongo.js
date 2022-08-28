9cc0b9de3c3485d861f7d437d8a852a2e114c157
The Lost Toastr <josht98@aol.com>
2022-08-28 12:00:08 -0400

    .

---
+++ mongodb/init-mongo.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user : "Joshtt23",
+        pwd : "Tyler03061998?!",
+        roles : [
+            {
+                role : "readWrite",
+                db : "Holders"
+            }
+        ]
+    }
+)

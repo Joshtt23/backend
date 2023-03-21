0047cfa819912d44234fdbcee47c55b9240b6c3c
The Lost Toastr <josht98@aol.com>
2023-03-21 12:18:54 -0400

    ssl updates

--- tf_backend/__main__.py
+++ tf_backend/__main__.py
@@ -10,7 +10,7 @@ from tf_backend.holders.WalletInfo import HolderChecker
 
 config = Config()
 config.bind=["0.0.0.0:8888"]
-config.certfile = "./tf_backend/api/certs/MyCert.pem"
+config.certfile = "./tf_backend/api/certs/ssl-bundle.crt"
 config.keyfile = "./tf_backend/api/certs/MyKey.pem"
 
 
